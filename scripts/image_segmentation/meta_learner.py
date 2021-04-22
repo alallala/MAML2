e2970ef00a6df252a70bce506f9e80ccc7f7673e
alallala <laura.gabrieleft@gmail.com>
2021-04-22 20:34:53 +0200

    m

--- scripts/image_segmentation/meta_learner.py
+++ scripts/image_segmentation/meta_learner.py
@@ -135,7 +135,7 @@ def build_unet(
 
     # extract skip connections
     skips = ([backbone.get_layers(name=i).output if isinstance(i, str)
-              else backbone.get_layer(index=i).output for i in ['block5_conv3', 'block4_conv3', 'block3_conv3', 'block2_conv2', 'block1_conv2']) # skip_connection_layers])
+              else backbone.get_layer(index=i).output for i in ['block5_conv3', 'block4_conv3', 'block3_conv3', 'block2_conv2', 'block1_conv2']]) # skip_connection_layers])
 
     # add center block if previous operation was maxpooling (for vgg models)
     if isinstance(backbone.layers[-1], layers.MaxPooling2D):
