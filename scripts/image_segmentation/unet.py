f6f7bbf636730a548d9a4ddb62b2612fbe3de86e
alallala <laura.gabrieleft@gmail.com>
2021-04-22 20:33:54 +0200

    m

--- scripts/image_segmentation/unet.py
+++ scripts/image_segmentation/unet.py
@@ -4,8 +4,6 @@ from .seg_commonblocks import Conv2dBn
 from .seg_utils import freeze_model, filter_keras_submodules
 from .seg_backbonesfactory.backbones_factory import Backbones
 
-import tensorflow.keras
-
 backend = None
 layers = None
 models = None
@@ -122,9 +120,9 @@ def build_unet(
     x = backbone.output
 
     # extract skip connections
-    #model = Model(input=base_model.input, output=base_model.get_layer('custom').output)
-    skips = ([backbone.layers(name=i).output if isinstance(i, str)
-              else backbone.layers(index=i).output for i in skip_connection_layers])
+    model = Model(input=base_model.input, output=base_model.get_layer('custom').output)
+    skips = ([backbone.get_layer(name=i).output if isinstance(i, str)
+              else backbone.get_layer(index=i).output for i in skip_connection_layers])
 
     # add center block if previous operation was maxpooling (for vgg models)
     if isinstance(backbone.layers[-1], layers.MaxPooling2D):
