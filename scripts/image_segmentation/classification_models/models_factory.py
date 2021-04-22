18b5991f06640a296d89ee79edcdc7b201491fe2
alallala <laura.gabrieleft@gmail.com>
2021-04-22 13:39:41 +0200

    m

--- scripts/image_segmentation/classification_models/models_factory.py
+++ scripts/image_segmentation/classification_models/models_factory.py
@@ -3,9 +3,9 @@ import tensorflow
 
 import tensorflow.keras.applications as ka
 
-from models import resnet as rn
-from models import resnext as rx
-from models import senet as sn
+from .models import resnet as rn
+from .models import resnext as rx
+from .models import senet as sn
 
 
 class ModelsFactory:
