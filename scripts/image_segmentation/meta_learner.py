ff673b180df253331a78c4d1ec70d6633354a0d3
alallala <laura.gabrieleft@gmail.com>
2021-04-23 17:00:33 +0200

    mod

--- scripts/image_segmentation/meta_learner.py
+++ scripts/image_segmentation/meta_learner.py
@@ -271,7 +271,7 @@ class MetaLearner():
             else:
                 new_weights.append(copied_model.weights[i])
         
-        copied_model.set_weights(np.asarray(new_weights,dytpe=object))
+        copied_model.set_weights(np.asarray(new_weights,dtype=object))
      
 
         return copied_model
