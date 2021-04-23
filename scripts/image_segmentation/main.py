43793ffde3ff1b3ba78afea5666bb495e5a18b67
alallala <laura.gabrieleft@gmail.com>
2021-04-23 16:43:40 +0200

    mod

--- scripts/image_segmentation/main.py
+++ scripts/image_segmentation/main.py
@@ -256,7 +256,7 @@ def maml_train(model, batch_generator):
                         i_w = ml.inner_weights(copied_model)
                     inner_grads = inner_tape.gradient(inner_loss, i_w)
                     print("NUMBER OF GRADS: ",len(inner_grads))
-                    copied_model = ml.meta_update(model=copied_model, args=args, alpha=inner_lr, grads=inner_grads[1:])
+                    copied_model = ml.meta_update(model=copied_model, args=args, alpha=inner_lr, grads=inner_grads)
                     
                 # Compute task loss & accuracy on the query set
                 task_loss, task_pred = compute_loss(copied_model, query_x, query_y) #, loss_fn=loss_fn)
