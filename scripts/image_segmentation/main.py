1bb41d71239449e628c9d0bfd9f58f6f38e3fc03
alallala <laura.gabrieleft@gmail.com>
2021-04-24 19:04:40 +0200

    mod

--- scripts/image_segmentation/main.py
+++ scripts/image_segmentation/main.py
@@ -502,7 +502,7 @@ if __name__ == '__main__':
     print ('Build model\n')
     model = ml.initialize_Unet()
     model = ml.initialize(model) 
-    print(model.summary)
+    print(model.summary())
     # tf.keras.utils.plot_model(model, to_file='../model.png',show_shapes=True,show_layer_names=True,dpi=128)
     # Initialize task generator
     print("\ntasks generation\n")
