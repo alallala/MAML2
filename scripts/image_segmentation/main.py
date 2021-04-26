891462850defac28c587ae4d1b4f9ff879c6c490
alallala <laura.gabrieleft@gmail.com>
2021-04-26 12:18:59 +0200

    mod

--- scripts/image_segmentation/main.py
+++ scripts/image_segmentation/main.py
@@ -19,6 +19,35 @@ from meta_learner import MetaLearner
 from losses import BinaryCELoss
 
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+ 
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import gen_image_ops
+from tensorflow.python.ops import math_ops
+ 
+
+@ops.RegisterGradient("ResizeBilinear")
+def _ResizeBilinearGrad(op, grad):
+  """The derivatives for bilinear resizing.
+
+  Args:
+    op: The ResizeBilinear op.
+    grad: The tensor representing the gradient w.r.t. the output.
+  Returns:
+    The gradients w.r.t. the input.
+  """
+  grad0 = gen_image_ops.resize_bilinear_grad(
+      grad,
+      op.inputs[0],
+      align_corners=op.get_attr("align_corners"),
+      half_pixel_centers=op.get_attr("half_pixel_centers"))
+  return [grad0, None]
+
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 '''
