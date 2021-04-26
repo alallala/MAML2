a8cee8d37d6781afa2507cbf6c226e3728c08781
alallala <laura.gabrieleft@gmail.com>
2021-04-26 12:19:51 +0200

    mod

--- scripts/image_segmentation/main.py
+++ scripts/image_segmentation/main.py
@@ -17,12 +17,9 @@ import matplotlib.pyplot as plt
 from task_generator import TaskGenerator
 from meta_learner import MetaLearner
 from losses import BinaryCELoss
-
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
- 
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
 from tensorflow.python.ops import array_ops
