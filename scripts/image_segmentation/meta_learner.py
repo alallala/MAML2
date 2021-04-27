a622911b026f510736f7885a2eb8dcab19cb0d08
alallala <laura.gabrieleft@gmail.com>
2021-04-27 12:16:13 +0200

    mod

--- scripts/image_segmentation/meta_learner.py
+++ scripts/image_segmentation/meta_learner.py
@@ -239,7 +239,7 @@ class MetaLearner():
 
     def initialize(cls,model):
     
-        ip_size = (5,128,128,3)
+        ip_size = (5,256,256,3)
         model.build(ip_size)
         
         return model
@@ -281,13 +281,14 @@ class MetaLearner():
             model.get_layer("final_conv").kernel, model.get_layer("final_conv").bias 
         ]
 
+       
         return weights
         
         
     def hard_copy(cls,model):
         
         copied_model = cls.initialize_Unet()
-        copied_model.build((5,128,128,3))
+        copied_model.build((5,256,256,3))
         
         copied_model.get_layer("block1_conv1").kernel = model.get_layer("block1_conv1").kernel 
         copied_model.get_layer("block1_conv1").bias = model.get_layer("block1_conv1").bias
@@ -417,7 +418,7 @@ class MetaLearner():
 
         copied_model = cls.initialize_Unet()
         
-        copied_model.build((5,128,128,3)) 
+        copied_model.build((5,256,256,3)) 
         
 
         #copied_model = keras.models.clone_model(model_to_copy)
