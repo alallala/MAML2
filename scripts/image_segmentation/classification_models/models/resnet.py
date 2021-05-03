c2d201f57e8a75b387babde162d1c9b9d8c5702e
alallala <laura.gabrieleft@gmail.com>
2021-05-03 14:03:07 +0200

    mod

--- scripts/image_segmentation/classification_models/models/resnet.py
+++ scripts/image_segmentation/classification_models/models/resnet.py
@@ -4,19 +4,8 @@ import tensorflow.keras as keras
 
 from classification_models.models._common_blocks import ChannelSE
 from classification_models.weights import load_model_weights
+     
 
-def get_submodules_from_kwargs(kwargs):
-    backend = keras.backend
-    layers = keras.layers
-    models = keras.models
-    utils = keras.utils
-    
-    return backend, layers, models, utils
-    
-backend = keras.backend
-layers = keras.layers
-models = keras.models
-keras_utils = keras.utils
 
 ModelParams = collections.namedtuple(
     'ModelParams',
