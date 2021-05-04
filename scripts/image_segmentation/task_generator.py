9c06244e9772408d967837160617a8aa7567b0bb
alallala <laura.gabrieleft@gmail.com>
2021-05-04 18:54:44 +0200

    mod

--- scripts/image_segmentation/task_generator.py
+++ scripts/image_segmentation/task_generator.py
@@ -74,6 +74,7 @@ def autoencoder_and_cluster(loaded_images):
    
     #prepare data to train the autoencoder
     x_train = fit_images[:800,:,:,:]
+    print(x_train.shape)
     x_train = x_train.reshape(len(x_train),input_shape[0],input[1],input_shape[2])
     x_train = resize(x_train, (128, 128))
     x_val = fit_images[800:1000,:,:,:]
