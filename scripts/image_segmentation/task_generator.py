b8cf6bc1ec66ecb2baaac788c1b8964c8eb3fa81
alallala <laura.gabrieleft@gmail.com>
2021-05-05 12:11:18 +0200

    mod

--- scripts/image_segmentation/task_generator.py
+++ scripts/image_segmentation/task_generator.py
@@ -118,12 +118,14 @@ def autoencoder_and_cluster(loaded_images):
     ae_model.compile(optimizer='adam', loss='binary_crossentropy')
     
     #model train
-    ae_model.fit(x_train, x_train, epochs=50, batch_size=64, validation_data=(x_val, x_val), verbose=1)
+    ae_model.fit(x_train, x_train, epochs=100, batch_size=64, validation_data=(x_val, x_val), verbose=1)
     
     #perform dimensionality reduction on train dataset to be used for segmentation 
     
-    '''
+    
     encoded_imgs = encoder.predict(x_train[1000:,:,:,])
+    print(encoded_imgs.shape)
+    '''
     encoded_imgs = encoded_imgs.reshape(-1,4096)
     '''
     
